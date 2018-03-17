54a4331889a7ced503eca1688d55c106c90bf58c
Eduardo <eduardok.fx@gmail.com>
2018-03-17 15:12:00 -0300

    Final Aula 1

--- Aula/ViewController.swift
+++ Aula/ViewController.swift
@@ -16,7 +16,8 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
     @IBAction func calcular() {
         let valorGasolina = (campoGasolina.text! as NSString).doubleValue;
         let valorEtanol = (campoEtanol.text! as NSString).doubleValue;
-        if(valorGasolina <= 0 || valorEtanol <= 0){
+        if(valorGasolina <= 0 || valorEtanol <= 0)
+        {
             showMessage(titulo: "ALERTA", mensagem: "Valores inválidos");
         }
         if(valorEtanol <= (valorGasolina*0.7))
@@ -41,34 +42,43 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
         let whatsappAction = UIAlertAction(title: "WhatsApp", style: .default){action -> Void in self.compartilharWhatsapp();}
         let emailAction = UIAlertAction(title:"Email", style: .default){ action -> Void in self.compartilharEmail();}
         let voltarAction = UIAlertAction(title: "Voltar", style: .cancel, handler: nil)
-        
+        let exibirAction = UIAlertAction(title: "EXIBIR", style: .default){ action -> Void in self.showMessage(titulo: "Alerta",mensagem: self.mensagemCompartilhar()) }
         
         alerta.addAction(emailAction)
         alerta.addAction(whatsappAction)
         alerta.addAction(voltarAction)
+        alerta.addAction(exibirAction)
+        
         
         self.present(alerta, animated: true, completion: nil)
     }
     
     func mensagemCompartilhar()->String{
-        return "VALOR GASOLINA: \(campoGasolina.text).  VALOR ETANOL \(campoEtanol.text)"
+        return "VALOR GASOLINA: \(campoGasolina.text!).  VALOR ETANOL \(campoEtanol.text!)"
     }
     
     func compartilharEmail(){
-        if (MFMailComposeViewController.canSendMail()){
+        if (MFMailComposeViewController.canSendMail())
+        {
             let compose = MFMailComposeViewController()
             compose.mailComposeDelegate = self
-            
             compose.setToRecipients(["eduardok.fx@gmail.com"])
             compose.setSubject("Assunto")
             compose.setMessageBody("Mensagem: \(mensagemCompartilhar())", isHTML: true)
+            compose.addAttachmentData(UIImageJPEGRepresentation(UIImage(named:"imagem.jpg")!, CGFloat(1.0))!, mimeType: "image/jpeg", fileName: "imagem.jpg")
+            self.present(compose, animated: true, completion: nil)
         }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
     }
     
     func compartilharWhatsapp(){
         let link = "whatsapp://send?text="
         
-        if let chamaLink = URL(string: link){
+        if let chamaLink = URL(string: link)
+        {
             UIApplication.shared.open(chamaLink, options: [:], completionHandler: nil)
         }
     }
